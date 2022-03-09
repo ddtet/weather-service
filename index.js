8954581b0871275fae2c1356c440a4e6342e0a64
Jeff Lin <jeffddt.et@gmail.com>
2022-03-09 22:53:10 +0800

    filter data by city name

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const process = require('process');
-const fetch = require('node-fetch');
+
+const getWeatherData = require('./lib/GetWeaterData');
+
+const DB_FILE = 'weather.db';
 
 // get the auth key from first cli param or env var
 const key = process.argv[2] || process.env.CWB_AUTH_KEY;
 
-// try to get the weather data
-async function getData(key) {
-    const response = await fetch(`https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/O-A0003-001?Authorization=${key}&downloadType=WEB&format=JSON`);
-
-    // output for check
-    console.log(await response.text());
-}
 
 // if no auth key input, don't exec fetch
-if ( key ) {
-    getData(key);
+if (key) {
+    getWeatherData(key)
+        .then(data => {
+            console.log(data);
+        });
 } else {
     console.error('you should set apiKey for get weather data.');
 }
