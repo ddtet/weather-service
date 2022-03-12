5f8fd92dfac2860e36e520b5fe9b45966252026a
Jeff Lin <jeffddt.et@gmail.com>
2022-03-12 10:19:49 +0800

    install express and try on

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const process = require('process');
+const express = require('express');
 
 const getWeatherData = require('./lib/GetWeaterData');
 
@@ -9,11 +10,30 @@ const key = process.argv[2] || process.env.CWB_AUTH_KEY;
 
 
 // if no auth key input, don't exec fetch
+let weatherData = null;
 if (key) {
     getWeatherData(key)
         .then(data => {
             console.log(data);
+            weatherData = data;
         });
 } else {
     console.error('you should set apiKey for get weather data.');
 }
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.end('api : weatherData'); 
+});
+
+app.get('/weatherData', (req, res) => {
+    console.log(req.query.authKey);
+    console.log('got request!');
+    res.type('json');
+    res.send(weatherData);
+})
+
+app.listen(80, () => {
+    console.log('http service started...');
+});
