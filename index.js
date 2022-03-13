1d6db0d45c7e33e2d1a0d0eaccb2fd2e63c43d82
Jeff Lin <jeffddt.et@gmail.com>
2022-03-13 16:30:19 +0800

    add comments

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const process = require('process');
 const path = require('path');
 
+// Get / Save Weather Data from DB (sqlite)
 const WeatherDao = require('./lib/WeatherDao');
-
+// Fetch Watcher data from other web api
 const WeatherDataGetter = require('./lib/WeatherDataGetter');
-
+// Privide http api for other
 const WebService = require('./lib/WebService');
 
+// sqlite file path
 const DB_FILE = 'weather.db';
 
 // get the auth key from first cli param or env var
@@ -17,15 +19,17 @@ if (!key) {
     process.exit(1);
 }
 
-const weatherDao = new WeatherDao(path.resolve(__dirname, DB_FILE));
 let weatherDataGetter;
 let webService;
+const weatherDao = new WeatherDao(path.resolve(__dirname, DB_FILE));
 
+// after dao (init db, get last data time), start data getter and web service
 weatherDao.init().then(() => {
     weatherDataGetter = new WeatherDataGetter(key, weatherDao);
     webService = new WebService(weatherDao);
 });
 
+// When Ctrl+C or process be killed, end the cron from get weather data, and close sqlite db
 process.on('SIGINT', () => {
     if (weatherDataGetter) {
         weatherDataGetter.stop();
